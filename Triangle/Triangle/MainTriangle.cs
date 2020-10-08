bd9d8051a8bc2eb426f8459eb7cb770b377e3a3b
Neha Ladani <Nladani8680@conestogac.on.ca>
2020-10-08 14:04:31 -0400

    Seventh change commit - The exception handling

--- Triangle/Triangle/MainTriangle.cs
+++ Triangle/Triangle/MainTriangle.cs
@@ -4,34 +4,43 @@ using System.Text;
 
 namespace Triangle
 {
-    class MainTriangle
+     public class MainTriangle
     {
         public static void Main()
         {
-            while (true)
+            try
             {
+                while (true)
+                {
                     Console.WriteLine($"1. Enter Triangle Dimensions ");
                     Console.WriteLine($"2. Exit");
                     int i = int.Parse(Console.ReadLine());
-                if (i == 1)
-                {
-                    Console.WriteLine($"Enter the side1 of the triangle ");
-                    int a = int.Parse(Console.ReadLine());
-                    Console.WriteLine($"Enter the side2 of the triangle");
-                    int b = int.Parse(Console.ReadLine());
-                    Console.WriteLine($"Enter the side3 of the triangle ");
-                    int c = int.Parse(Console.ReadLine());
-                    Console.WriteLine(TriangleSolver.Analyze(a, b, c));
-                    break;
+                    if (i == 1)
+                    {
+                        Console.WriteLine($"Enter the side1 of the triangle ");
+                        int a = int.Parse(Console.ReadLine());
+                        Console.WriteLine($"Enter the side2 of the triangle");
+                        int b = int.Parse(Console.ReadLine());
+                        Console.WriteLine($"Enter the side3 of the triangle ");
+                        int c = int.Parse(Console.ReadLine());
+                        Console.WriteLine(TriangleSolver.Analyze(a, b, c));
+                        break;
+                    }
+
+                    else if (i == 2)
+                    {
+                        break;
+                    }
                 }
-            
-                else if (i == 2)
-                {
-                     break;
-                }
+            }
+            catch
+            {
+                Console.WriteLine($"Dimensions entered are incorrect");
+            }
 
         }
-            }
-        }
     }
+}
+        
+    
 
