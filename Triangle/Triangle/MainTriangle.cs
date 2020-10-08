046e90346a32de6d3b15d55db366747ff09c26f7
Neha Ladani <Nladani8680@conestogac.on.ca>
2020-10-08 16:38:07 -0400

    Fifteenth change commit - Added few comments

--- Triangle/Triangle/MainTriangle.cs
+++ Triangle/Triangle/MainTriangle.cs
@@ -5,38 +5,62 @@ using System.Data;
 
 namespace Triangle
 {
+    // Creating class
      public class MainTriangle
     {
+        // Main method
         public static void Main()
         {
+            // Exception Handling - If entering double value instead of integers.
             try
             {
+                // Checks the condition for the infinity loop.
                 while (true)
                 {
+                    // Prints the line
                     Console.WriteLine($"1. Enter Triangle Dimensions ");
+                    // Prints the line
                     Console.WriteLine($"2. Exit");
+
+                    // Initialize the variable for the user input.
                     // Node A:
                     int i = int.Parse(Console.ReadLine());
+
+                    // Condition for the infinity loop. 
+                    // If user enters input value as '1' then the condition satisfies and performs the condition.
                     // Node B:
                     if (i == 1)
                     {
+                        // Prints the line.
                         // Node C;
                         Console.WriteLine($"Enter the side1 of the triangle ");
+                        // User can input side1 of the triangle value.
                         // Node D:
                         int a = int.Parse(Console.ReadLine());
+
+                        // Prints the line
                         // Node E:
                         Console.WriteLine($"Enter the side2 of the triangle");
+                        // User can input side2 of the triangle value.
                         // Node F:
                         int b = int.Parse(Console.ReadLine());
+
+                        // Prints the line
                         // Node G:
                         Console.WriteLine($"Enter the side3 of the triangle ");
+                        // User can input side3 of the triangle value.
                         // Node H:
                         int c = int.Parse(Console.ReadLine());
+
+                        // Call the Analyze method to print which triangle satisfies the given value and displays the kind of the triangle accordingly.
                         // Node I:
                         Console.WriteLine(TriangleSolver.Analyze(a, b, c));
+
+                        // Procedure stops
                         break;
                     }
 
+                    // Exit
                     // Node S:
                     else if (i == 2)
                     {
@@ -44,6 +68,7 @@ namespace Triangle
                     }
                 }
             }
+            // Exception Handling and prints the reason about what's wrong.
             catch
             {
                 Console.WriteLine($"Dimensions entered are incorrect");
--- Triangle/Triangle/TriangleSolver.cs
+++ Triangle/Triangle/TriangleSolver.cs
@@ -2,35 +2,48 @@
 
 namespace Triangle
 {
+    //creating class
      public class TriangleSolver
     {
+        // Creating method and passing parameters with return type string because the output of the program should return string.
         public static string Analyze(int a, int b, int c)
         {
+            // Basic condition of a triangle - checks whether the given values form a triangle or not.
             // Node J:
             if (a + b > c && b + c > a && c + a > b == true)
             {
+                // Prints - if the given values satisfies the condition.
                 // Node K:
                 Console.WriteLine($"It is a Triangle");
 
+                // Condition checks whether the given values forms an Equilateral triangle - having all sides equal.
                 // Node L:
                 if (a == b && b == c && c == a)
                 {
+                    // Prints - if the given values satisfies the condition.
+                    // It returns string, so return type is used.
                     // Node M:
                     return "It is an Equilateral Triangle";
                 }
+
+                // Condition checks whether the given values forms an Isosceles triangle - having any two sides equal.
                 // Node N:
                 else if (a == b || b == c || a == c)
                 {
+                    // Prints - if the given values satisfies the condition.
                     // Node O:
                     return "It is an Isosceles Triangle";
                 }
+
+                // If the condition becomes false - 
                 // Node P:
                 else
                 {
+                    // Then prints
                     return "It is a Scalene Triangle";
                 } // Node Q:
-
             }
+            // If the basic condition is not satisfied, then it directly prints that it is not a triangle.
             // Node R:
             else
             {
