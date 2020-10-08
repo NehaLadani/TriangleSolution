8055a39352856b663ca5717c033da62a571039c6
Neha Ladani <Nladani8680@conestogac.on.ca>
2020-10-08 14:01:59 -0400

    Sixth change commit - The conditions applied to the program

--- Triangle/Triangle/MainTriangle.cs
+++ Triangle/Triangle/MainTriangle.cs
@@ -13,11 +13,25 @@ namespace Triangle
                     Console.WriteLine($"1. Enter Triangle Dimensions ");
                     Console.WriteLine($"2. Exit");
                     int i = int.Parse(Console.ReadLine());
-                    
+                if (i == 1)
                 {
-
+                    Console.WriteLine($"Enter the side1 of the triangle ");
+                    int a = int.Parse(Console.ReadLine());
+                    Console.WriteLine($"Enter the side2 of the triangle");
+                    int b = int.Parse(Console.ReadLine());
+                    Console.WriteLine($"Enter the side3 of the triangle ");
+                    int c = int.Parse(Console.ReadLine());
+                    Console.WriteLine(TriangleSolver.Analyze(a, b, c));
+                    break;
                 }
+            
+                else if (i == 2)
+                {
+                     break;
+                }
+
+        }
             }
         }
     }
-}
+
