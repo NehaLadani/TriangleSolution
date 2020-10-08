64dcf68d03ac4e3ff0984bfb31763edc5067e041
Neha Ladani <Nladani8680@conestogac.on.ca>
2020-10-08 16:00:44 -0400

    Fourteenth change commit - Added nodes of CFG

--- Triangle/Triangle/MainTriangle.cs
+++ Triangle/Triangle/MainTriangle.cs
@@ -15,19 +15,29 @@ namespace Triangle
                 {
                     Console.WriteLine($"1. Enter Triangle Dimensions ");
                     Console.WriteLine($"2. Exit");
+                    // Node A:
                     int i = int.Parse(Console.ReadLine());
+                    // Node B:
                     if (i == 1)
                     {
+                        // Node C;
                         Console.WriteLine($"Enter the side1 of the triangle ");
+                        // Node D:
                         int a = int.Parse(Console.ReadLine());
+                        // Node E:
                         Console.WriteLine($"Enter the side2 of the triangle");
+                        // Node F:
                         int b = int.Parse(Console.ReadLine());
+                        // Node G:
                         Console.WriteLine($"Enter the side3 of the triangle ");
+                        // Node H:
                         int c = int.Parse(Console.ReadLine());
+                        // Node I:
                         Console.WriteLine(TriangleSolver.Analyze(a, b, c));
                         break;
                     }
 
+                    // Node S:
                     else if (i == 2)
                     {
                         break;
--- Triangle/Triangle/TriangleSolver.cs
+++ Triangle/Triangle/TriangleSolver.cs
@@ -6,23 +6,32 @@ namespace Triangle
     {
         public static string Analyze(int a, int b, int c)
         {
+            // Node J:
             if (a + b > c && b + c > a && c + a > b == true)
             {
+                // Node K:
                 Console.WriteLine($"It is a Triangle");
 
+                // Node L:
                 if (a == b && b == c && c == a)
                 {
+                    // Node M:
                     return "It is an Equilateral Triangle";
                 }
+                // Node N:
                 else if (a == b || b == c || a == c)
                 {
+                    // Node O:
                     return "It is an Isosceles Triangle";
                 }
+                // Node P:
                 else
                 {
                     return "It is a Scalene Triangle";
-                }
+                } // Node Q:
+
             }
+            // Node R:
             else
             {
                 return "It is not a Triangle";
