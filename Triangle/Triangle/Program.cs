bade35b2510353dc8a8fd98d8ea762c7b028d7bc
Neha Ladani <Nladani8680@conestogac.on.ca>
2020-10-08 13:55:09 -0400

    Fourth change commit - Written conditions and the string to be printed.

--- Triangle/Triangle/Program.cs
+++ Triangle/Triangle/Program.cs
@@ -4,12 +4,34 @@ namespace Triangle
 {
     class TriangleSolver
     {
-        public static void Analyze(int a, int b, int c)
+        public static static Analyze(int a, int b, int c)
         {
-            if(a + b > c && b + c > a && c + a > b == true)
+            if (a + b > c && b + c > a && c + a > b == true)
             {
                 Console.WriteLine($"It is a Triangle");
+
+                if (a == b && b == c && c == a)
+                {
+                    return "It is an Equilateral Triangle";
+                }
+                else if (a == b || b == c || a == c)
+                {
+                    return "It is an Isosceles Triangle";
+                }
+                else
+                {
+                    return "It is a Scalene Triangle";
+                }
+            }
+            else
+            {
+                return "It is not a Triangle";
+                //Console.WriteLine($" It is not a Triangle ");
             }
         }
     }
 }
+
+
+    
+
