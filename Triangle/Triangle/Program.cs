efa9bb8c77dae2c4d9d4acf3994bff38095ee4a5
Neha Ladani <Nladani8680@conestogac.on.ca>
2020-10-08 14:06:15 -0400

    Eighth change commit - To handle error and made code to be executed.

--- Triangle/Triangle/Program.cs
+++ Triangle/Triangle/Program.cs
@@ -2,9 +2,9 @@
 
 namespace Triangle
 {
-    class TriangleSolver
+     public class TriangleSolver
     {
-        public static static Analyze(int a, int b, int c)
+        public static string Analyze(int a, int b, int c)
         {
             if (a + b > c && b + c > a && c + a > b == true)
             {
